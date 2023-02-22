8d43cb5fff81ef7ffeabc2ba42a947d74b2ce7af
Adib Nasreddine <eddynasreddine@hotmail.com>
2023-02-21 22:25:36 -0700

    Custom adapter for each qrcode in the listview for the users profile screen

--- app/src/main/java/com/example/qrrush/ProfileAdapter.java
+++ app/src/main/java/com/example/qrrush/ProfileAdapter.java
@@ -5,13 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
-
+// Custom Adapter for each item in the listview
 public class ProfileAdapter extends ArrayAdapter<QRcode> {
+
     public ProfileAdapter(Context context, ArrayList<QRcode> objects) {
         super(context, 0, objects);
     }
@@ -25,7 +29,18 @@ public class ProfileAdapter extends ArrayAdapter<QRcode> {
         } else {
             view = convertView;
         }
-
+        QRcode qRcode = super.getItem(position);
+        TextView nameView = view.findViewById(R.id.nameView);
+        TextView pointView = view.findViewById(R.id.pointView);
+        TextView locationView = view.findViewById(R.id.locationView);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        nameView.setText(qRcode.getName());
+        pointView.setText(String.valueOf(qRcode.getValue()));
+        locationView.setText(qRcode.getLocation());
+        Picasso
+                .get()
+                .load(qRcode.getImageURL())
+                .into(imageView);
         return view;
     }
 }
