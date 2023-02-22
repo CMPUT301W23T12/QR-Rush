3d73cebd51ca9b170f6a212fcc41c2ad97714cfb
Musa Abdullahi <mose1@ualberta.ca>
2023-02-21 20:59:24 -0700

    added the generating QR functionality

---
+++ app/src/main/java/com/example/qrrush/Score.java
@@ -0,0 +1,65 @@
+package com.example.qrrush;
+
+import java.util.Random;
+
+public class Score {
+    private int scoreValue;
+
+    public Score(int scoreValue) {
+        this.scoreValue = scoreValue;
+    }
+
+    public int getScoreValue() {
+        return scoreValue;
+    }
+
+    public void setScoreValue(int scoreValue) {
+        this.scoreValue = scoreValue;
+    }
+
+    public static int calculateScore(String hashValue, int numZeroes) {
+        if (numZeroes >= 4) {
+            return generateLegendaryScore();
+        } else if (numZeroes >= 2) {
+            return generateRareScore();
+        } else {
+            return generateCommonScore();
+        }
+    }
+
+    public static int generateCommonScore() {
+        return new Random().nextInt(10) + 1;
+    }
+
+    public static int generateRareScore() {
+        return new Random().nextInt(40) + 11;
+    }
+
+    public static int generateLegendaryScore() {
+        return new Random().nextInt(50) + 51;
+    }
+
+    public static int getNumConsecutiveZeroes(String hashValue) {
+        int numConsecutiveZeroes = 0;
+        int maxConsecutiveZeroes = 0;
+        for (int i = 0; i < hashValue.length(); i++) {
+            if (hashValue.charAt(i) == '0') {
+                numConsecutiveZeroes++;
+                maxConsecutiveZeroes = Math.max(maxConsecutiveZeroes, numConsecutiveZeroes);
+            } else {
+                numConsecutiveZeroes = 0;
+            }
+        }
+        return maxConsecutiveZeroes;
+    }
+    public static int getRarity(String hashValue) {
+        int numZeroes = getNumConsecutiveZeroes(hashValue);
+        if (numZeroes >= 4) {
+            return 2; // Legendary
+        } else if (numZeroes >= 2) {
+            return 1; // Rare
+        } else {
+            return 0; // Common
+        }
+    }
+}
--- app/src/main/java/com/example/qrrush/ShopFragment.java
+++ app/src/main/java/com/example/qrrush/ShopFragment.java
@@ -1,12 +1,24 @@
 package com.example.qrrush;
 
+import static com.example.qrrush.Score.getNumConsecutiveZeroes;
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.qrrush.Score;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +34,15 @@ public class ShopFragment extends Fragment {
 
     // TODO: Rename and change types of parameters
     private String mParam1;
+    private Random rand = new Random();
     private String mParam2;
+    private ImageView qrCode;
+    private TextView scoreText;
+    private Button commonButton;
+    private Button rareButton;
+    private Button legendaryButton;
+    private Score score;
+    private TextView qrContentText;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -55,10 +75,133 @@ public class ShopFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false);
+        View view = inflater.inflate(R.layout.fragment_shop, container, false);
+
+        // Now you can call findViewById on the inflated view
+        qrCode = view.findViewById(R.id.qr_code);
+        scoreText = view.findViewById(R.id.score);
+        commonButton = view.findViewById(R.id.common_button);
+        rareButton = view.findViewById(R.id.rare_button);
+        legendaryButton = view.findViewById(R.id.legendary_button);
+        score = new Score(0);
+        qrContentText = view.findViewById(R.id.qr_content);
+
+        // Add click listeners to buttons
+        commonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Generate hash, calculate score, and update views
+                String hashValue = generateHash(1);
+                int commonScore = score.calculateScore(hashValue, 1);
+                scoreText.setText("Score: " + commonScore);
+                qrContentText.setText("QR content: " + hashValue);
+                Log.d("MainActivity", "Current Score: " + commonScore);
+            }
+        });
+
+        rareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Generate hash, calculate score, and update views
+                String hashValue = generateHash(2);
+                int rareScore = Score.generateRareScore();
+                scoreText.setText("Score: " + rareScore);
+                qrContentText.setText("QR content: " + hashValue);
+                Log.d("MainActivity", "Current Score: " + rareScore);
+            }
+        });
+
+        legendaryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Generate hash, calculate score, and update views
+                String hashValue = generateHash(4);
+                int legendaryScore = Score.generateLegendaryScore();
+                scoreText.setText("Score: " + legendaryScore);
+                qrContentText.setText("QR content: " + hashValue);
+                Log.d("MainActivity", "Current Score: " + legendaryScore);
+            }
+        });
+
+        return view;
+    }
+
+    public static String generateHash(int numZeroes) {
+        Random rand = new Random();
+        String hashValue;
+        while (true) {
+            StringBuilder sb = new StringBuilder(32);
+            for (int i = 0; i < 32; i++) {
+                sb.append(Integer.toHexString(rand.nextInt(16)));
+            }
+            String str = sb.toString();
+
+            hashValue = sha256(str);
+            int scoreValue = 0;
+            if (numZeroes >= 4) {
+                // Legendary
+                if (getNumConsecutiveZeroes(hashValue) >= 4) {
+                    scoreValue = Score.generateLegendaryScore();
+                } else {
+                    // Choose a random index to insert the zeroes
+                    int index = rand.nextInt(28) + 2; // exclude first and last two characters
+                    // Replace a substring of length 4 with four zeroes
+                    hashValue = hashValue.substring(0, index) + "0000" + hashValue.substring(index + 4);
+                    scoreValue = Score.generateLegendaryScore();
+                }
+            } else if (numZeroes >= 2) {
+                // Rare
+                int index = rand.nextInt(30); // choose a random index to insert zeroes
+                String substring = hashValue.substring(index, index + 2); // get a 2 character substring
+                if (substring.contains("0")) {
+                    continue; // the substring already contains a zero, start over
+                } else {
+                    // insert zeroes into the substring
+                    String newSubstring = substring.substring(0, rand.nextInt(1)) + "00"
+                            + substring.substring(rand.nextInt(1) + 1, 2);
+                    hashValue = hashValue.substring(0, index) + newSubstring
+                            + hashValue.substring(index + 2);
+                    scoreValue = Score.generateRareScore();
+                }
+            } else {
+                // Common
+                if (getNumConsecutiveZeroes(hashValue) == 0) {
+                    scoreValue = Score.generateCommonScore();
+                } else {
+                    continue;
+                }
+            }
+
+            // Check if the score matches and return the hash if it does
+            int calculatedScore = Score.calculateScore(hashValue, numZeroes);
+            if (calculatedScore == scoreValue) {
+                return hashValue;
+            }
+        }
+    }
+
+    private static String sha256(String str) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
