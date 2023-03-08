c27b43fa443879919925f5d7d5f7c7f2bbc06f4f
Ayman El Didi <ayman@eldidi.org>
2023-03-07 22:59:01 -0700

    began work on visual representation

--- app/src/main/java/com/example/qrrush/QRCode.java
+++ app/src/main/java/com/example/qrrush/QRCode.java
@@ -1,8 +1,12 @@
 package com.example.qrrush;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Random;
 
@@ -14,6 +18,7 @@ public class QRCode {
 
     /**
      * Makes a new QRCode from some data without specifying a Location.
+     *
      * @param data The data contained inside the QR Code.
      */
     public QRCode(byte[] data) {
@@ -33,7 +38,8 @@ public class QRCode {
 
     /**
      * Makes a new QRCode from some data, specifying the Location which the QR Code was scanned.
-     * @param data The data contained inside the QR Code.
+     *
+     * @param data     The data contained inside the QR Code.
      * @param location The location which the QR Code was scanned.
      */
     public QRCode(byte[] data, String location) {
@@ -49,6 +55,7 @@ public class QRCode {
 
     /**
      * Converts some array of bytes to a hex string.
+     *
      * @param bytes The array of bytes to convert to a hex string.
      */
     private static String bytesToHex(byte[] bytes) {
@@ -61,6 +68,7 @@ public class QRCode {
 
     /**
      * Returns a new QR Code of a specified rarity.
+     *
      * @param r The rarity of the QR Code to generate.
      */
     public static QRCode withRarity(Rarity r) {
@@ -75,7 +83,7 @@ public class QRCode {
         for (int i = 0; i < numZeroes; i += 1) {
             data[i] = '0';
         }
-        
+
         QRCode result = null;
 
         // If the score isn't high enough, keep adding more fs to the hash, since that's the
@@ -89,7 +97,7 @@ public class QRCode {
 
             data[i] = 'f';
         }
-        
+
         return result;
     }
 
@@ -101,8 +109,47 @@ public class QRCode {
         return location;
     }
 
-    public String getImageURL() {
-        return imageURL;
+    public Canvas getImage() {
+        // 1. For each character of the hash, if it's even make it a 0, and if its odd make it a 1.
+        StringBuilder binaryHash = new StringBuilder(this.hash);
+        for (int i = 0; i < this.hash.length(); i += 1) {
+            byte[] val = {(byte) (Integer.parseInt(Character.toString(binaryHash.charAt(i)), 16) % 2)};
+            binaryHash.setCharAt(i, bytesToHex(val).charAt(0));
+        }
+
+        // 2. Add 4 extra padding characters to make it a square by dividing up the hash into 4
+        //    sections and performing the same thing as 1 on the sum of each of the sections.
+        String[] sections = {
+                this.hash.substring(0, 7),
+                this.hash.substring(8, 16),
+                this.hash.substring(17, 25),
+                this.hash.substring(26, 32),
+        };
+        for (String section : sections) {
+            ArrayList<Integer> bytes = new ArrayList<>(8);
+            for (int i = 0; i < this.hash.length(); i += 1) {
+                bytes.add((int) Integer.parseInt(Character.toString(binaryHash.charAt(i)), 16));
+            }
+
+            int sum = 0;
+            for (int i : bytes) {
+                sum += i;
+            }
+
+            byte[] val = {(byte) (sum % 2)};
+            binaryHash.append(bytesToHex(val).charAt(0));
+        }
+
+        // 3. Use the first 3 bytes of the hash as RGB values for the color.
+        int c = Color.rgb(
+                Integer.parseInt(Character.toString(binaryHash.charAt(0)), 16),
+                Integer.parseInt(Character.toString(binaryHash.charAt(1)), 16),
+                Integer.parseInt(Character.toString(binaryHash.charAt(2)), 16)
+        );
+
+        // 4. Paint each pixel into a 6x6 square.
+        // TODO: actually do this.
+        return new Canvas();
     }
 
     public static int getMaxConsecutiveZeroes(String hashValue) {
