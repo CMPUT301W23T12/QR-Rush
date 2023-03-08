a5ef70e62080a24fdd0ecf3918009993e041294a
Adib Nasreddine <eddynasreddine@hotmail.com>
2023-03-07 18:45:04 -0700

    Removed the users information from this class to MainActivity

--- app/src/main/java/com/example/qrrush/ProfileFragment.java
+++ app/src/main/java/com/example/qrrush/ProfileFragment.java
@@ -20,8 +20,13 @@ public class ProfileFragment extends Fragment {
     User user;
     QRCodeAdapter QRCodeAdapter;
 
-    public ProfileFragment() {
+    /**
+     * Grabs User object from the main activity
+     * @param user
+     */
+    public ProfileFragment(User user) {
         // Required empty public constructor
+        this.user = user;
     }
 
     @Override
@@ -41,23 +46,10 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        /**
+         * Sets up initial profile
+         */
         super.onViewCreated(view, savedInstanceState);
-
-        ArrayList<QRCode> qrCodes = new ArrayList<>();
-        byte[][] sampleData = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9},
-        };
-        qrCodes.add(new QRCode(sampleData[0]));
-        qrCodes.add(new QRCode(sampleData[1]));
-        qrCodes.add(new QRCode(sampleData[2]));
-
-        // Sets the users profile, still need to crop the image to fit a certain size
-        // TODO: remove copyrighted material before merging into main.
-        user = new User("TheLegend27",
-                "987-6543-321", 1, qrCodes,
-                "https://static.wikia.nocookie.net/intothespiderverse/images/b/b0/Wilson_Fisk_%28E-1610%29_001.png/revision/latest?cb=20210609163717");
         TextView contactView = view.findViewById(R.id.contactView);
         TextView nameView = view.findViewById(R.id.nameView);
         TextView rankView = view.findViewById(R.id.rankView);
@@ -67,19 +59,21 @@ public class ProfileFragment extends Fragment {
         contactView.setText(user.getPhoneNumber());
         nameView.setText(user.getUserName());
         rankView.setText(String.valueOf(user.getRank()));
-        scoreView.setText(String.valueOf(user.getScore()));
+        scoreView.setText(String.valueOf(user.getTotalScore()));
         QRScanned.setText(String.valueOf(user.getNumberOfQRCodes()));
-
-
-        // Image will be fit into the size of the image view
+        /**
+         *      Image will be fit into the size of the image view
+         */
         Picasso
                 .get()
                 .load(user.getProfilePicture())
                 .fit()
                 .into(profileView);
 
-
-        QRCodeAdapter = new QRCodeAdapter(requireActivity(), user.getQRCodes(),user);
+        /**
+         * Passes User object from main activity to the QR code adapter
+         */
+        QRCodeAdapter = new QRCodeAdapter(requireActivity(), user.getQRCodes(), user);
         ListView qrCodeList = view.findViewById(R.id.listy);
         qrCodeList.setAdapter(QRCodeAdapter);
         QRCodeAdapter.notifyDataSetChanged();
