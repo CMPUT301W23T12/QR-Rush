a6bc817ac3a688dc4be2087245cd9c45450e0052
Ayman El Didi <ayman@eldidi.org>
2023-03-09 13:04:22 -0700

    minor refactoring of SocialFragment

--- app/src/main/java/com/example/qrrush/SocialFragment.java
+++ app/src/main/java/com/example/qrrush/SocialFragment.java
@@ -19,15 +19,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 
 public class SocialFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     User user;
 
     public SocialFragment(User user) {
@@ -37,10 +28,6 @@ public class SocialFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -67,41 +54,42 @@ public class SocialFragment extends Fragment {
                 TextView searchedRank = view.findViewById(R.id.rankView);
                 String searchPlayer = searchPlayerEditField.getText().toString();
 
-                if (!searchPlayer.matches("")) {
-                    FirebaseWrapper.getUserData(searchPlayer, new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if (task.isSuccessful()) {
-                                DocumentSnapshot document = task.getResult();
-                                if (document.exists()) {
-                                    // Player Search is found, Display the Profile
-                                    noPlayerFound.setVisibility(View.GONE);
-                                    searchedName.setText(searchPlayer);
-                                    searchedRank.setText(String.format("%d", document.getLong("rank").intValue()));
-                                    searchedName.setVisibility(View.VISIBLE);
-                                    searchedRank.setVisibility(View.VISIBLE);
-                                    PlayerImg.setVisibility(View.VISIBLE);
-
-                                } else {
-                                    // Player is not found, display the "No player found!"
-                                    noPlayerFound.setVisibility(View.VISIBLE);
-                                    searchedName.setVisibility(View.GONE);
-                                    PlayerImg.setVisibility(View.GONE);
-                                    searchedRank.setVisibility(View.GONE);
-                                }
-                            } else {
-                                Log.e("SearchingPlayer", "Error fetching usernames in firebase");
-                            }
-
-                        }
-                    });
-                } else {
+                if (searchPlayer.matches("")) {
                     // UX Experience: If search is empty and you press search, it will display "No player found!"
                     noPlayerFound.setVisibility(View.VISIBLE);
                     searchedName.setVisibility(View.GONE);
                     PlayerImg.setVisibility(View.GONE);
                     searchedRank.setVisibility(View.GONE);
+                    return;
                 }
+
+                FirebaseWrapper.getUserData(searchPlayer, new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("SearchingPlayer", "Error fetching usernames in firebase");
+                            return;
+                        }
+
+                        DocumentSnapshot document = task.getResult();
+                        if (!document.exists()) {
+                            // Player is not found, display the "No player found!"
+                            noPlayerFound.setVisibility(View.VISIBLE);
+                            searchedName.setVisibility(View.GONE);
+                            PlayerImg.setVisibility(View.GONE);
+                            searchedRank.setVisibility(View.GONE);
+                            return;
+                        }
+
+                        // Player Search is found, Display the Profile
+                        noPlayerFound.setVisibility(View.GONE);
+                        searchedName.setText(searchPlayer);
+                        searchedRank.setText(String.format("%d", document.getLong("rank").intValue()));
+                        searchedName.setVisibility(View.VISIBLE);
+                        searchedRank.setVisibility(View.VISIBLE);
+                        PlayerImg.setVisibility(View.VISIBLE);
+                    }
+                });
 
             }
         });
