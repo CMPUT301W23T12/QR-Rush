3b8aed6182006ea8e03f2b5043c672b6205648fe
Adib Nasreddine <eddynasreddine@hotmail.com>
2023-03-09 07:35:13 -0700

    Custom comparator for QR code score

--- app/src/main/java/com/example/qrrush/ScoreComparator.java
+++ app/src/main/java/com/example/qrrush/ScoreComparator.java
@@ -1,2 +1,13 @@
-package com.example.qrrush;public class ScoreComparator {
+package com.example.qrrush;
+
+import java.util.Comparator;
+
+/**
+ * Custom Comparator to sort the QR codes by score
+ */
+public class ScoreComparator implements Comparator<QRCode> {
+    @Override
+    public int compare(QRCode QR1, QRCode QR2) {
+        return Integer.compare(QR2.getScore(), QR1.getScore());
+    }
 }
