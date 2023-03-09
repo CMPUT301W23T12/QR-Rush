89ea1c71ffca1d13fc1057c8f49a9ade43ff6837
Abdullahi Nur <abdul.nur2828@gmail.com>
2023-03-09 15:09:38 -0700

    allowing user to change username

--- app/src/main/java/com/example/qrrush/ProfileFragment.java
+++ app/src/main/java/com/example/qrrush/ProfileFragment.java
@@ -1,20 +1,28 @@
 package com.example.qrrush;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class ProfileFragment extends Fragment {
@@ -75,5 +83,74 @@ public class ProfileFragment extends Fragment {
         ListView qrCodeList = view.findViewById(R.id.listy);
         qrCodeList.setAdapter(QRCodeAdapter);
         QRCodeAdapter.notifyDataSetChanged();
+
+        Button editNameButton = view.findViewById(R.id.edit_name); // Get the button view from the layout
+
+        editNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View addNewName = getLayoutInflater().inflate(R.layout.profile_overlay, null);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireActivity());
+                alertDialogBuilder.setView(addNewName);
+                alertDialogBuilder.setMessage("Input New Name:");
+                alertDialogBuilder.setTitle("Alert!");
+
+                alertDialogBuilder.setPositiveButton("Confirm", null);
+
+                AlertDialog dialog = alertDialogBuilder.create();
+                dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialogInterface) {
+                        //add a positive button on the alertdialog that tells user to confirm their input
+                        Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                        button.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // store the new name input of the user
+                                EditText userNameEdit = addNewName.findViewById(R.id.input_new_name);
+                                String newUserName = userNameEdit.getText().toString();
+
+                                FirebaseWrapper.getUserData(UserUtil.getUsername(requireContext().getApplicationContext()), new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(Task<DocumentSnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            DocumentSnapshot document = task.getResult();
+                                            if (document.exists()) // if username is present
+                                            {
+                                                /// Retrieve all the fields from the Database of that user
+                                                //String newName = "Bob";
+                                                String phonenumber = document.getString("phone-number");
+                                                int rank = document.getLong("rank").intValue();
+                                                int score = document.getLong("score").intValue();
+                                                HashMap<String, Object> updatedProfile = new HashMap<>();
+                                                updatedProfile.put("name", newUserName);
+                                                updatedProfile.put("phone-number", phonenumber);
+                                                updatedProfile.put("rank", rank);
+                                                updatedProfile.put("score", score);
+                                                // Check if username is already taken, if so do not add it to firebase
+                                                FirebaseWrapper.addData("profiles", "UpdatedName", updatedProfile);
+                                                FirebaseWrapper.removeUserProfile(UserUtil.getUsername(requireContext().getApplicationContext()));
+                                                UserUtil.getUsername(requireContext().getApplicationContext(),newUserName);
+                                                user = new User(newUserName,
+                                                        document.getString("phone-number"),
+                                                        document.getLong("rank").intValue(),
+                                                        document.getLong("score").intValue(),
+                                                        qrCodeList);
+
+                                            } else {
+
+                                            }
+                                        } else {
+
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
     }
 }
