db77ada8f8b4b4c2ea3bf2bbb46f79ce05ad197a
Abdullahi Nur <abdul.nur2828@gmail.com>
2023-03-12 16:10:14 -0600

    working on profileTest

--- app/src/androidTest/java/com/example/qrrush/ProfileTest.java
+++ app/src/androidTest/java/com/example/qrrush/ProfileTest.java
@@ -1,10 +1,14 @@
 package com.example.qrrush;
 
+
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -19,7 +23,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 public class ProfileTest {
-    private User user1;
+    //public User user = new User("attn", "123")
     private Solo solo; // An instance of Solo to simulate user interactions
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class, true, false);
@@ -48,18 +52,30 @@ public class ProfileTest {
         solo.clickOnView(solo.getView(R.id.common_button));
         solo.clickOnView(solo.getView(R.id.rare_button));
         solo.clickOnView(solo.getView(R.id.profile_button));
-        int totalScore = user1.getTotalScore();
-        int numQRcodes = user1.getNumberOfQRCodes();
-        int countQR = 0;
-        int countScore = 0;
-        ArrayList<QRCode> qrCodes = user1.getQRCodes();
-        for (QRCode qrCode : qrCodes) {
-            countScore += qrCode.getScore();
-            countQR += 1;
-        }
 
-        assertTrue(countQR == numQRcodes);
-        assertTrue(countScore == totalScore);
+        // Get the QR code count view
+        TextView qrCountView = (TextView) solo.getView(R.id.qrCodesView);
+
+        // Get the QR code count value
+        int qrCount = Integer.parseInt(qrCountView.getText().toString());
+
+        // Compare the QR code count value to the expected value
+        assertEquals(2, qrCount);
+
+
+
+//        int totalScore = user1.getTotalScore();
+//        int numQRcodes = user1.getNumberOfQRCodes();
+//        int countQR = 0;
+//        int countScore = 0;
+//        ArrayList<QRCode> qrCodes = user1.getQRCodes();
+//        for (QRCode qrCode : qrCodes) {
+//            countScore += qrCode.getScore();
+//            countQR += 1;
+//        }
+//
+//        assertTrue(countQR == numQRcodes);
+//        assertTrue(countScore == totalScore);
 
 
     }
