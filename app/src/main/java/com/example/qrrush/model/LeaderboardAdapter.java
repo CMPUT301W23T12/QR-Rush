68704404ec804e41f9d6ed3bba4914b3b9b4fb3a
Adib Nasreddine <eddynasreddine@hotmail.com>
2023-03-14 22:17:07 -0600

    custom adapter for leaderboard to display other players

---
+++ app/src/main/java/com/example/qrrush/model/LeaderboardAdapter.java
@@ -0,0 +1,47 @@
+package com.example.qrrush.model;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.qrrush.R;
+
+import java.util.ArrayList;
+
+/**
+ * Custom adapter that will display the high scores of all players
+ */
+public class LeaderboardAdapter extends ArrayAdapter<Player> {
+    ArrayList<Player> players;
+
+
+    /**
+     * Creates a LeaderboardAdapter, unsure of datatype for now.
+     * @param context The context object to pass to the super constructor.
+     */
+    public LeaderboardAdapter(Context context,ArrayList<Player> objects) {
+        super(context, 0, objects);
+        players = objects;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (convertView == null) {
+            view = LayoutInflater.from(super.getContext()).inflate(R.layout.leaderboard_content, parent, false);
+        }
+        Player player = getItem(position);
+
+        TextView OtherUserNameView = view.findViewById(R.id.OtherUserNameView);
+        TextView OtherUserScoreView = view.findViewById(R.id.OtherUserScoreView);
+        OtherUserNameView.setText(player.getName());
+        OtherUserScoreView.setText(String.valueOf(player.getScore()));
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/qrrush/model/Player.java
@@ -0,0 +1,2 @@
+package com.example.qrrush.model;public class Player {
+}
