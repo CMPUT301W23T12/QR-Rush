561e533208dfa6cf3b0af4f10159bef7e748c220
Adib Nasreddine <eddynasreddine@hotmail.com>
2023-03-14 22:18:48 -0600

    New player class, may be deleted or changed in the future

--- app/src/main/java/com/example/qrrush/model/Player.java
+++ app/src/main/java/com/example/qrrush/model/Player.java
@@ -1,2 +1,29 @@
-package com.example.qrrush.model;public class Player {
+package com.example.qrrush.model;
+
+public class Player {
+    String name;
+    int score;
+    // unsure for now
+    String Image;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
