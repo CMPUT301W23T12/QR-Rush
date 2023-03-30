d59c95218140bf4f501753034e164c741c0f61df
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2023-03-30 15:45:25 -0600

    testing adding location images

--- app/src/main/java/com/example/qrrush/view/QRConfirmFragment.java
+++ app/src/main/java/com/example/qrrush/view/QRConfirmFragment.java
@@ -1,7 +1,13 @@
 package com.example.qrrush.view;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +16,8 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -19,7 +27,14 @@ import com.example.qrrush.R;
 import com.example.qrrush.model.Geo;
 import com.example.qrrush.model.QRCode;
 import com.example.qrrush.model.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.mlkit.vision.barcode.common.Barcode;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * this class is responsible for
@@ -38,6 +53,13 @@ public class QRConfirmFragment extends DialogFragment {
     TextView locationView;
     CheckBox geolocationToggle;
     FragmentManager manager;
+    Button foundLocationButton;
+    ImageView locationImage;
+
+    private static final int CAMERA_REQUEST_CODE = 100;
+
+
+    private ActivityResultLauncher<Intent> cameraLauncher;
 
     public QRConfirmFragment(User user, Barcode b) {
         this.user = user;
@@ -61,8 +83,11 @@ public class QRConfirmFragment extends DialogFragment {
         rarityView = view.findViewById(R.id.qrCode_rarity);
         locationView = view.findViewById(R.id.qrCode_location);
         geolocationToggle = view.findViewById(R.id.geolocation_checkbox);
+        foundLocationButton = view.findViewById(R.id.foundLocation);
+        locationImage = view.findViewById(R.id.locationImage);
 
         QRCode qrCode = new QRCode(this.code.getRawBytes());
+
 
         retakeButton.setOnClickListener(v -> {
             new CameraFragment(user).show(manager, "Confirm QR code");
@@ -75,6 +100,17 @@ public class QRConfirmFragment extends DialogFragment {
                 dismiss();
             });
         });
+
+        foundLocationButton.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            // Start the camera activity and wait for the result
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+
+
+
+
+        });
+
 
         Bitmap b = Bitmap.createScaledBitmap(qrCode.getImage(), 200, 200, false);
         qrCodeImage.setImageBitmap(b);
@@ -108,4 +144,24 @@ public class QRConfirmFragment extends DialogFragment {
         manager = getParentFragmentManager();
         return result;
     }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            // Get the image data from the intent
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+
+            // Display the image in an ImageView
+            locationImage.setVisibility(View.VISIBLE);
+            locationImage.setImageBitmap(photo);
+
+
+        }
+    }
+
+
 }
